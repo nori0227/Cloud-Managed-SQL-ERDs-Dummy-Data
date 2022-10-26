547f7b815ff7f64884137c15272a354985befcbe
Jannatul Ferdous <jannatul.ferdous@stonybrook.edu>
2022-10-26 01:20:54 -0400

    new

--- sql_dummy_data.py
+++ sql_dummy_data.py
@@ -1,1 +1,43 @@
 
+import dbm
+import pandas as pd
+import sqlalchemy
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+from faker import Faker  # https://faker.readthedocs.io/en/master/
+import uuid
+import random
+
+
+AZURE_MYSQL_HOSTNAME = os.getenv("AZURE_MYSQL_HOSTNAME")
+AZURE_MYSQL_USER = os.getenv("AZURE_MYSQL_USER")
+AZURE_MYSQL_PASSWORD = os.getenv("AZURE_MYSQL_PASSWORD")
+AZURE_MYSQL_DATABASE = os.getenv("AZURE_MYSQL_DATABASE")
+
+
+connection_string_azure = f'mysql+pymysql://{AZURE_MYSQL_USER}:{AZURE_MYSQL_PASSWORD}@{AZURE_MYSQL_HOSTNAME}:3306/{AZURE_MYSQL_DATABASE}'
+db_azure = create_engine(connection_string_azure)
+
+# show databases
+print(db_azure.table_names())
+
+# fake stuff
+fake = Faker()
+
+fake_patients = [
+    {
+        # keep just the first 8 characters of the uuid
+        'mrn': str(uuid.uuid4())[:8],
+        'first_name':fake.first_name(),
+        'last_name':fake.last_name(),
+        'zip_code':fake.zipcode(),
+        'dob':(fake.date_between(start_date='-90y', end_date='-20y')).strftime("%Y-%m-%d"),
+        'gender': fake.random_element(elements=('M', 'F')),
+        'contact_mobile':fake.phone_number(),
+        'contact_home':fake.phone_number()
+    } for x in range(10)]
+
+df_fake_patients = pd.DataFrame(fake_patients)
+# drop duplicate mrn
+df_fake_patients = df_fake_patients.drop_duplicates(subset=['mrn'])
