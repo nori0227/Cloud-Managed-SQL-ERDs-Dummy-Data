5bcb46ba2f16f0566392c3326d7da121d7d1d640
Jannatul Ferdous <jannatul.ferdous@stonybrook.edu>
2022-10-26 01:08:01 -0400

    new changes

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Cloud-Managed-SQL-ERDs-Dummy-Data
 HHA 504 Assignment 6
+
+
+## Need to have a .env file in the root directory with following structure for Part5_Db:
+```
+AZURE_MYSQL_HOSTNAME = "inserthere"
+AZURE_MYSQL_USERNAME = "inserthere"
+AZURE_MYSQL_PASSWORD = "inserthere"
+AZURE_MYSQL_DATABASE = "inserthere"
+```
--- sql_table_creation.py
+++ sql_table_creation.py
@@ -1,1 +1,89 @@
 
+import dbm
+import pandas as pd 
+import sqlalchemy
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+####  CONNECT TO AZURE MYSQL SERVER  
+AZURE_MYSQL_HOSTNAME = os.getenv("AZURE_MYSQL_HOSTNAME")
+AZURE_MYSQL_USER = os.getenv("AZURE_MYSQL_USER")
+AZURE_MYSQL_PASSWORD = os.getenv("AZURE_MYSQL_PASSWORD")
+AZURE_MYSQL_DATABASE = os.getenv("AZURE_MYSQL_DATABASE")
+
+string_azure = f'mysql+pymysql://{AZURE_MYSQL_USER}:{AZURE_MYSQL_PASSWORD}@{AZURE_MYSQL_HOSTNAME}:3306/{AZURE_MYSQL_DATABASE}'
+db_azure = create_engine(string_azure)
+
+####  SHOW THE TABLES FROM THE DATABASE  
+tableNames_azure = db_azure.table_names()
+
+# reorder tables: patient_conditions, patient_procedure, patients, conditions
+tableNames_azure = ['patient_conditions', 'patient_procedure', 'patients', 'conditions']
+
+#### first step below is just creating a basic version of each of the tables,
+#### along with the primary keys and default values 
+
+
+table_patient_conditions = """
+create table if not exists patient_conditions (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    icd10_code varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (icd10_code) REFERENCES conditions(icd10_code) ON DELETE CASCADE
+); 
+"""
+
+
+table_patient_procedure = """
+create table if not exists patient_procedure (
+    id int auto_increment,
+    mrn varchar(255) default null,
+    proc_cpt varchar(255) default null,
+    PRIMARY KEY (id),
+    FOREIGN KEY (mrn) REFERENCES patients(mrn) ON DELETE CASCADE,
+    FOREIGN KEY (proc_cpt) REFERENCES sx_procedure(proc_cpt) ON DELETE CASCADE
+); 
+"""
+
+table_patients = """
+create table if not exists patients (
+    id int auto_increment,
+    mrn varchar(255) default null unique,
+    first_name varchar(255) default null,
+    last_name varchar(255) default null,
+    zip_code varchar(255) default null,
+    dob varchar(255) default null,
+    gender varchar(255) default null,
+    contact_mobile varchar(255) default null,
+    contact_home varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+table_conditions = """
+create table if not exists conditions (
+    id int auto_increment,
+    icd10_code varchar(255) default null unique,
+    icd10_desc varchar(255) default null,
+    PRIMARY KEY (id) 
+); 
+"""
+
+
+
+db_azure.execute(table_patient_conditions)
+db_azure.execute(table_patient_procedure)
+db_azure.execute(table_patients)
+db_azure.execute(table_conditions)
+
+
+
+# get tables from db_azure
+azure_tables = db_azure.table_names()
+
